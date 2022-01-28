9b6541c683050a31f43510e8839c517c966fb1a4
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2022-01-28 18:40:33 +0100

    Added functions to read out tbinfo and tbexec from hdf5 file
    - Added generic access function that will load a table from an experiment
    - Added generic access function that reinflates dataset with goldenrundata
    - Added access functions to tbinfo and tbexec that use the generic function in the background

--- analysis/analysisfunctions.py
+++ analysis/analysisfunctions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def generate_groupname_list(faultgroup):
     """
     Generator to get names of all childs in faultgroup
@@ -183,7 +185,7 @@ def get_faultgroup_configuration(faultgroup, name):
     """
     fault = {}
     node = faultgroup._f_get_child(name)
-    fault["faults"] = node.faults.read()
+    fault["faults"] = pd.DataFrame(node.faults.read()).to_dict()
     fault["index"] = int(name[10:])
     return fault
 
@@ -198,3 +200,44 @@ def get_complete_faultconfiguration(filehandle, interestlist = None):
     for faultname in interestlist:
         faultconfiguration.append(get_faultgroup_configuration(faultfolder, faultname))
     return faultconfiguration
+
+def get_experiment_table(faultgroup, faultname, tablename):
+    """
+    Get anny table from a faultgroup
+    """
+    node = faultgroup._f_get_child(faultname)
+    table = node._f_get_child(tablename)
+    return pd.DataFrame(table.read())
+
+def get_experiment_table_expanded(filehandle, faultname, tablename, keyword):
+    """
+    Get the experiment table and recombine it with the goldenrun data
+    TODO: This data must be checked if not to much is included
+    """
+    golden_table = get_experiment_table(filehandle.root, "Goldenrun", tablename)
+    exp_table = get_experiment_table(filehandle.root.fault, faultname, tablename)
+    tmp = exp_table[keyword]
+    idx = None
+    for t in tmp:
+        idt = golden_table.index[golden_table[keyword] == t]
+        if idx is None:
+            idx = idt
+        else:
+            idx.append(idt)
+    if idx is not None:
+        golden_table.drop(idx, inplace=True)
+    data = [exp_table, golden_table]
+    return pd.concat(data).to_dict("records")
+
+def get_experiment_tbinfo(faultgroup, faultname):
+    return get_experiment_table(faultgroup, faultname, "tbinfo")
+
+def get_experiment_tbinfo_expanded(filehandle, faultname):
+    return get_experiment_table_expanded(filehandle, faultname, "tbinfo", "identity")
+
+def get_experiment_tbexec(faultgroup, faultname):
+    return get_experiment_table(faultgroup, faultname, "tbexeclist")
+
+def get_experiment_tbexec_expanded(filehandle, faultname):
+    return get_experiment_table_expanded(filehandle, faultname, "tbexeclist", "pos")
+
