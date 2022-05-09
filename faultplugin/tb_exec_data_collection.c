aa769e567c4f8bdd2618193a76aca9e3fd17ecdc
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2022-05-09 23:38:47 +0200

    faultplugin: add ring buffer store for the TB execution list

--- faultplugin/tb_exec_data_collection.c
+++ faultplugin/tb_exec_data_collection.c
@@ -20,15 +20,39 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+extern int tb_exec_order_ring_buffer;
+
 tb_exec_order_t *tb_exec_order_list;
 
 uint64_t num_exec_order;
 
-void tb_exec_order_init()
+struct tb_exec_rb_element {
+	tb_info_t *tb_info;
+	uint64_t pos;
+};
+
+#define TB_EXEC_RB_SIZE 100
+struct tb_exec_rb_element *tb_exec_rb_list = NULL;
+int tb_exec_rb_list_index;
+
+int tb_exec_order_init()
 {
 	// List of execution order of tbs.
 	tb_exec_order_list = NULL;
 	num_exec_order = 0;
+
+	if (tb_exec_order_ring_buffer)
+	{
+		tb_exec_rb_list_index = 0;
+		tb_exec_rb_list = (struct tb_exec_rb_element *)malloc(
+			TB_EXEC_RB_SIZE * sizeof(struct tb_exec_rb_element));
+		if (tb_exec_rb_list == NULL)
+		{
+			return -1;
+		}
+	}
+
+	return 0;
 }
 
 
@@ -47,6 +71,11 @@ void tb_exec_order_free()
 		tb_exec_order_list = tb_exec_order_list->prev;
 		free(item);
 	}
+
+	if (tb_exec_order_ring_buffer && tb_exec_rb_list != NULL)
+	{
+		free(tb_exec_rb_list);
+	}
 }
 
 
@@ -58,39 +87,76 @@ void tb_exec_order_free()
 void plugin_dump_tb_exec_order()
 {
 	uint64_t i = 0;
-	if(tb_exec_order_list == NULL)
-	{
-		return;
-	}
 	g_autoptr(GString) out = g_string_new("");
 	g_string_printf(out, "$$$[TB Exec]:\n");
 	plugin_write_to_data_pipe(out->str, out->len);
-	tb_exec_order_t *item =  tb_exec_order_list;
 
-	while(item->prev != NULL)
+	if (tb_exec_order_ring_buffer)
 	{
-		i++;
-		item = item->prev;
-	}
-	i++;
-	if(i != num_exec_order)
-	{
-		qemu_plugin_outs("[WARNING]: i and numexec differ!\n");
-	}
-	i = 0;
-	while(item != NULL)
-	{
-		if(item->tb_info == NULL)
+		/*
+		 * If we logged less than TB_EXEC_RB_SIZE, the start of the buffer is
+		 * at index 0. Otherwise, it is stored in tb_exec_rb_list_index.
+		 */
+		if (num_exec_order >= TB_EXEC_RB_SIZE)
 		{
-			g_string_printf(out, "$$ 0x0000 | %li \n", i);
+			i = tb_exec_rb_list_index;
 		}
-		else
+
+		for (int j = 0; j < TB_EXEC_RB_SIZE && j < num_exec_order; j++)
 		{
-			g_string_printf(out, "$$ 0x%lx | %li \n", item->tb_info->base_address, i);
+			if (tb_exec_rb_list[i].tb_info == NULL)
+			{
+				g_string_printf(out, "$$ 0x0000 | %li \n", tb_exec_rb_list[i].pos);
+			}
+			else
+			{
+				g_string_printf(out, "$$ 0x%lx | %li \n",
+								tb_exec_rb_list[i].tb_info->base_address,
+								tb_exec_rb_list[i].pos);
+			}
+			plugin_write_to_data_pipe(out->str, out->len);
+
+			i++;
+			if (i == TB_EXEC_RB_SIZE)
+			{
+				i = 0;
+			}
 		}
-		plugin_write_to_data_pipe(out->str, out->len);
-		item = item->next;
+	}
+	else
+	{
+		tb_exec_order_t *item =  tb_exec_order_list;
+
+		if(item == NULL)
+		{
+			return;
+		}
+
+		while(item->prev != NULL)
+		{
+			i++;
+			item = item->prev;
+		}
 		i++;
+		if(i != num_exec_order)
+		{
+			qemu_plugin_outs("[WARNING]: i and numexec differ!\n");
+		}
+		i = 0;
+		while(item != NULL)
+		{
+			if(item->tb_info == NULL)
+			{
+				g_string_printf(out, "$$ 0x0000 | %li \n", i);
+			}
+			else
+			{
+				g_string_printf(out, "$$ 0x%lx | %li \n", item->tb_info->base_address, i);
+			}
+			plugin_write_to_data_pipe(out->str, out->len);
+			item = item->next;
+			i++;
+		}
 	}
 }
 
@@ -109,14 +175,30 @@ void tb_exec_data_event(unsigned int vcpu_index, void *vcurrent)
 	{
 		tb_info->num_of_exec++;
 	}
-	tb_exec_order_t *last = malloc(sizeof(tb_exec_order_t));
-	last->tb_info = tb_info;
-	last->next = NULL;
-	last->prev = tb_exec_order_list;
-	if(tb_exec_order_list != NULL)
+
+	if (tb_exec_order_ring_buffer)
 	{
-		tb_exec_order_list->next = last;
+		tb_exec_rb_list[tb_exec_rb_list_index].tb_info = tb_info;
+		tb_exec_rb_list[tb_exec_rb_list_index].pos = num_exec_order;
+
+		tb_exec_rb_list_index++;
+		if (tb_exec_rb_list_index == TB_EXEC_RB_SIZE)
+		{
+			tb_exec_rb_list_index = 0;
+		}
 	}
-	tb_exec_order_list = last;
+	else
+	{
+		tb_exec_order_t *last = malloc(sizeof(tb_exec_order_t));
+		last->tb_info = tb_info;
+		last->next = NULL;
+		last->prev = tb_exec_order_list;
+		if(tb_exec_order_list != NULL)
+		{
+			tb_exec_order_list->next = last;
+		}
+		tb_exec_order_list = last;
+	}
+
 	num_exec_order++;
 }
