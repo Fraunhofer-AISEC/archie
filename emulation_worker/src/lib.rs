4682095c8be40280787213672ef2af1f89f495df
Kevin Schneider <kevin.j.sneijder@gmail.com>
2023-01-27 18:16:31 +0100

    add unicorn emulation worker with meminfo logging and pregoldenrun initialization

---
+++ emulation_worker/src/lib.rs
@@ -0,0 +1,83 @@
+use priority_queue::PriorityQueue;
+use pyo3::{
+    prelude::*,
+    types::{PyDict, PyList},
+};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
+use unicorn_engine::Unicorn;
+use unicorn_engine::unicorn_const::{
+    Arch,
+    Mode,
+    Permission,
+};
+
+mod arm;
+use crate::arm::initialize_arm_registers;
+
+mod logs;
+use crate::logs::{Logs, MemInfo};
+
+mod hooks;
+use crate::hooks::initialize_hooks;
+
+#[pyfunction]
+fn run_unicorn(pregoldenrun_data: &PyDict, config: &PyDict) -> PyResult<PyObject> {
+
+    println!("{:?}", config);
+    let mut unicorn = Unicorn::new(Arch::ARM, Mode::THUMB).expect("failed to initialize Unicorn instance");
+    let emu = &mut unicorn;
+
+    let memdumplist: &PyList = pregoldenrun_data
+        .get_item("memdumplist")
+        .unwrap()
+        .extract()?;
+    for obj in memdumplist.iter() {
+        let memdump: &PyDict = obj.extract()?;
+        let address: u64 = memdump.get_item("address").unwrap().extract()?;
+        let length: usize = memdump.get_item("len").unwrap().extract()?;
+        let dumps: &PyList = memdump.get_item("dumps").unwrap().extract()?;
+        let dump: Vec<u8> = dumps.get_item(0).unwrap().extract()?;
+
+        // TODO: Use correct permissions
+        emu.mem_map(address, length, Permission::ALL)
+            .expect("failed to map code page");
+        emu.mem_write(address, dump.as_slice())
+            .expect("failed to write instructions");
+    }
+
+    let armregisters: &PyList = pregoldenrun_data
+        .get_item("armregisters")
+        .unwrap()
+        .extract()?;
+    let registerdump: &PyDict = armregisters.get_item(0).unwrap().extract()?;
+
+    initialize_arm_registers(emu, registerdump);
+
+    let logs = Logs {
+        meminfo: RwLock::new(HashMap::new()),
+        last_tbid: RwLock::new(0),
+        endpoints: RwLock::new(HashMap::new())
+    };
+    let logs_arc : Arc<Logs> = Arc::new(logs);
+
+    initialize_hooks(emu, &logs_arc, config).expect("failed initializing hooks");
+
+    let max_instruction_count: usize = config
+        .get_item("max_instruction_count")
+        .unwrap()
+        .extract()?;
+    let start: HashMap<String, u64> = config.get_item("start").unwrap().extract()?;
+    emu.emu_start(*start.get("address").unwrap()+1, 0, 0, max_instruction_count).expect("failed to emulate code");
+
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    Ok(logs_arc.to_object(py))
+}
+
+#[pymodule]
+fn emulation_worker(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(run_unicorn, m)?)?;
+    Ok(())
+}
