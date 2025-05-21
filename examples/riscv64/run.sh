dbf0640fbbde0112db42c28b2d0ef79757d8c1f2
Kevin Schneider <kevin.j.sneijder@gmail.com>
2025-05-21 15:00:16 +0200

    include basic unicorn tests in ci pipeline

--- examples/riscv64/run.sh
+++ examples/riscv64/run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python3 ../../controller.py --debug --fault fault.json  --qemu qemuconf.json output.hdf5
+if [ "$1" = "--unicorn" ]; then
+    python3 ../../controller.py --debug --fault fault_unicorn.json --qemu qemuconf.json output.hdf5 --unicorn
+else
+    python3 ../../controller.py --debug --fault fault.json --qemu qemuconf.json output.hdf5
+fi
+
