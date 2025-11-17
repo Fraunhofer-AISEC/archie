1ae1d5831b0b12b8915cb54f80a2b3daccdc8324
Kevin Schneider <kevin.j.sneijder@gmail.com>
2025-11-17 15:51:58 +0100

    undo stm32-timeout-wfi run.sh changes

--- examples/stm32-timeout-wfi/run.sh
+++ examples/stm32-timeout-wfi/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 ../../controller.py --debug --fault fault.json  --qemu qemuconf.json output.hdf5 $1
+python3 ../../controller.py --debug --fault fault.json  --qemu qemuconf.json output.hdf5
